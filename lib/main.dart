92d212be916e9ca13c97b6ac890524bc259bd1d4
csonah <revelationjay02@gmail.com>
2022-04-08 22:31:01 +0100

    resolved android build issues

--- lib/main.dart
+++ lib/main.dart
@@ -7,4 +7,5 @@ void main() {
   //Register services
   setupLocator();
   runApp(Shuttla());
+
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,7 +9,6 @@ import firebase_auth
 import firebase_core
 import firebase_messaging
 import firebase_storage
-import geolocator_apple
 import package_info
 import shared_preferences_macos
 
@@ -18,7 +17,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
-  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FLTPackageInfoPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
