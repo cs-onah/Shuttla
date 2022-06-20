09475b51b4f6f16d5bf1361f505523615cc02d5b
Chukwuebuka Onah <revelationjay02@gmail.com>
2022-06-20 20:58:27 +0100

    created station model

---
+++ lib/core/data_models/station.dart
@@ -0,0 +1,71 @@
+// To parse this JSON data, do
+//
+//     final station = stationFromMap(jsonString);
+
+import 'dart:convert';
+
+class Station {
+  Station({
+    required this.stationName,
+    required this.coordinates,
+    required this.createdDate,
+    this.driverId,
+    this.driverName,
+    this.plateNumber,
+  });
+
+  String stationName;
+  List<double> coordinates;
+  String createdDate;
+  String? driverId;
+  String? driverName;
+  String? plateNumber;
+
+  Station copyWith({
+    String? stationName,
+    List<double>? coordinates,
+    String? createdDate,
+    String? driverId,
+    String? driverName,
+    String? plateNumber,
+  }) =>
+      Station(
+        stationName: stationName ?? this.stationName,
+        coordinates: coordinates ?? this.coordinates,
+        createdDate: createdDate ?? this.createdDate,
+        driverId: driverId ?? this.driverId,
+        driverName: driverName ?? this.driverName,
+        plateNumber: plateNumber ?? this.plateNumber,
+      );
+
+  factory Station.fromJson(String str) => Station.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory Station.fromMap(Map<String, dynamic> json) => Station(
+    stationName: json["station_name"],
+    coordinates: List<double>.from(json["coordinates"].map((x) => x.toDouble())),
+    createdDate: json["created_date"],
+    driverId: json["driver_id"],
+    driverName: json["driver_name"],
+    plateNumber: json["plate_number"],
+  );
+
+  Map<String, dynamic> toMap() => {
+    "station_name": stationName,
+    "coordinates": List<dynamic>.from(coordinates.map((x) => x)),
+    "created_date": createdDate,
+    "driver_id": driverId,
+    "driver_name": driverName,
+    "plate_number": plateNumber,
+  };
+}
+
+///{
+//     "station_name" : "hey",
+//     "coordinates" : [6.943, 8.126],
+//     "created_date" : "hdk",
+//     "driver_id" : "",
+//     "driver_name" : "",
+//     "plate_number" : "driver_plate_number"
+// }
