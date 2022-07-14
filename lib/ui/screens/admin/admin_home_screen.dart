5c4624490a5220ab4a312f4a4d138680536ff1dc
Chukwuebuka Onah <revelationjay02@gmail.com>
2022-07-14 18:32:13 +0100

    built admin home

--- lib/ui/screens/admin/admin_home_screen.dart
+++ lib/ui/screens/admin/admin_home_screen.dart
@@ -16,14 +16,15 @@ class _AdminHomeScreenState extends State<AdminHomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: Text("Stations",
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
+        title: Text(
+          "Stations",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
         ),
         backgroundColor: Colors.white,
         centerTitle: false,
@@ -36,6 +37,86 @@ class _AdminHomeScreenState extends State<AdminHomeScreen> {
               color: Colors.black,
             ),
           )
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: Align(
+              alignment: Alignment.topRight,
+              child: SizedBox(
+                height: 50,
+                child: TextButton.icon(
+                  onPressed: null,
+                  icon: Icon(
+                    Icons.add,
+                    color: Colors.white,
+                  ),
+                  label: Text(
+                    "New Station     ",
+                    style: TextStyle(fontSize: 14, color: Colors.white),
+                  ),
+                  style: TextButton.styleFrom(
+                    backgroundColor: Theme.of(context).primaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              padding: EdgeInsets.all(16),
+              shrinkWrap: true,
+              children: [
+
+                for(int i=0; i<5 ;i++)
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                    color: Colors.white,
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                  margin: EdgeInsets.only(bottom: 16),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Station Name",
+                              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                            ),
+                            SizedBox(height: 10),
+                            Row(
+                              children: [Icon(Icons.location_on_outlined), SizedBox(width: 5),
+                                Text("6.77754, 8.4397")
+                              ],
+                            ),
+                            Row(
+                              children: [Icon(Icons.person_outline), SizedBox(width: 5),
+                                Text("8 waiting")
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+
+                      IconButton(onPressed: (){}, icon: Icon(Icons.edit)),
+                      IconButton(onPressed: (){}, icon: Icon(Icons.delete), color: Colors.red),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
---
+++ lib/ui/screens/admin/create_station_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CreateStationScreen extends StatelessWidget {
+  const CreateStationScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
